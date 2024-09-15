6c98c13ed4f142e98b03cc7fda15b182c744bcc1
imariiana <marianavitorialima69@gmail.com>
2024-09-15 10:29:18 -0300

    :sparkles: feat: Efeitos e Cardapio

---
+++ reveal.js
@@ -0,0 +1,54 @@
+window.revelar = ScrollReveal({ reset: true })
+
+revelar.reveal('.item', {
+    duration: 2000,
+    distance:'30px'
+})
+
+//BOX DAS SECÇÕES DE ALIMENTOS 
+
+revelar.reveal('.container3,.subcontainer3', {
+    duration: 2000,
+    distance:'30px',
+    delay: 400
+})
+
+//SECÇÃO BOLOS E TORTAS
+
+revelar.reveal('.efeito-img-bolos', {
+    duration: 2000,
+    distance:'90px',
+})
+
+revelar.reveal('.efeito-txt1-bolos', {
+    duration: 2000,
+    distance:'30px',
+    delay:500
+})
+
+revelar.reveal('.efeito-txt2-bolos', {
+    duration: 2000,
+    distance:'30px',
+    delay:700
+})
+
+revelar.reveal('.efeito-direita-bolos', {
+    duration: 3000,
+    distance:'90px',
+    delay:500,
+    origin:'left'
+})
+
+revelar.reveal('.efeito-esquerda-bolos', {
+    duration: 3000,
+    distance:'90px',
+    delay:500,
+    origin:'right'
+})
+
+revelar.reveal('.sobre', {
+    duration: 2000,
+    distance:'30px',
+})
+
+
